23c68bc07756532cd57b677ab4598c276044319d
Rajatt-Chawla <your-email@example.com>
2025-11-10 13:52:59 +0530

    Fix: separated client and metadata in Solvoris page

--- src/app/products/solvoris/page.tsx
+++ src/app/products/solvoris/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next'
 import Image from 'next/image'
+import Link from 'next/link'
 import SolvorisModules from '@/components/products/SolvorisModules'
 
 export const metadata: Metadata = {
